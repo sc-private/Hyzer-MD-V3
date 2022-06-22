b6a79869af4ed3d11eb0dee18da77b64d88a562d
RyanStore <107747308+RyanStore@users.noreply.github.com>
2022-06-22 22:58:57 +0800

    Update main-ok.js

--- plugins/main-ok.js
+++ plugins/main-ok.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/bd6914e065dbd698e72d7.jpg', m, { packname: "sticker by", author: "ArullOfc" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/bd6914e065dbd698e72d7.jpg', m, { packname: "sticker by", author: "YannTuru" })
 }
 
 handler.customPrefix = /^(ok)$/i
