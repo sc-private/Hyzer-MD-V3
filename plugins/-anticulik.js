fb88f1fec882e442785a2bf7933bcf20e5ce4ece
RyanStore <107747308+RyanStore@users.noreply.github.com>
2022-06-22 22:24:32 +0800

    Update -anticulik.js

--- plugins/-anticulik.js
+++ plugins/-anticulik.js
@@ -2,16 +2,16 @@ let handler = m => handler.all = async function (m, { isAdmin, isBotAdmin }) {
   if (m.mtype == 'groupInviteMessage') {
 idnya = await this.reply(m.chat, `「 Donasi • Pulsa 」
 
-• *Gopay:* 
-• *Axis:* 6282350496532
+• *Gopay:082252285143* 
+• *Telkomsel:* 6282350496532
 • *Telkomsel:* 6282350496532
 
 Donasi semampunya untuk mendukung bot ini tetap hidup untuk kalian^_^
-Kalau gak mampu yang di atas bisa juga *follow ig* https://instagram.com/sahrulwara_____
+Kalau gak mampu yang di atas bisa juga *Download Apk Melalui Kode Referal Saya* https://toss.vn/s/rlqSFBzn
 
 Kontak pemilik saya⬇️⬇️
 `, m)
-this.sendContact(m.chat, '6285822347348', 'Pemilik saya', idnya)
+this.sendContact(m.chat, '6282252285144', 'Pemilik saya', idnya)
   }
   return true
 }
